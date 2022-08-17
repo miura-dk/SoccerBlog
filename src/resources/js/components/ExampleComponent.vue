d42e0d42609d40869cdef63397e83dae393f9ccb
miura-dk <miuradaiki03@gmail.com>
2022-08-17 21:42:57 +0900

    mdiアイコンインストール welcomeページ変更 ExampleComponent変更  Vuetify設定など #2

--- src/resources/js/components/ExampleComponent.vue
+++ src/resources/js/components/ExampleComponent.vue
@@ -1,17 +1,18 @@
 <template>
-    <div class="container">
-        <div class="row justify-content-center">
-            <div class="col-md-8">
-                <div class="card">
-                    <div class="card-header">Example Component</div>
-
-                    <div class="card-body">
-                        I'm an example component.
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
+    <v-app>
+        <v-main>
+            <v-container>Hello world</v-container>
+                <v-row>
+                    <v-col cols="6">
+                        <v-btn color="indigo darken-1">ボタン1</v-btn>
+                    </v-col>
+                    <v-col cols="6">
+                        <v-btn color="red">ボタン2</v-btn>
+                    </v-col>
+                </v-row>
+            
+        </v-main>
+    </v-app>
 </template>
 
 <script>
